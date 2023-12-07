0fedd139e661dd69845970ff94ef71336ee44f4d
“Reemy” <reem.onez@algooru.com>
2023-12-07 16:58:29 +0300

    urls file added

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,18 +1,18 @@
+import 'package:clean_project/helpers/constants/urls.dart';
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 GetIt getIt = GetIt.instance;
 
-void init() async{
-
+void init() async {
   final sharedPreferences = await SharedPreferences.getInstance();
   getIt.registerLazySingleton(() => sharedPreferences);
 
   getIt.registerLazySingleton(
     () => Dio(
       BaseOptions(
-        baseUrl: '', /// baseUrl according to flavor configuration,
+        baseUrl: Urls.baseUrl,
         contentType: 'application/json',
         followRedirects: false,
         validateStatus: (status) {
@@ -20,8 +20,8 @@ void init() async{
         },
         headers: {
           Headers.acceptHeader: "application/json",
-          'Accept-Language': '', /// read from sharedPreference
-          'Authorization': '', /// read from sharedPreference
+          //  'Accept-Language': '', /// read from sharedPreference
+          // 'Authorization': '', /// read from sharedPreference
         },
       ),
     ),
